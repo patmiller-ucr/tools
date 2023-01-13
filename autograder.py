b9c7a2413494a4cdfb50137b98a6f89b9252df5b
Jaired Jawed <jairedjawed@icloud.com>
2023-01-12 16:25:19 -0800

    run cpp file and print output

--- autograder.py
+++ autograder.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 
 # parse cpp file name and --test file from command line
 parser = argparse.ArgumentParser(description='Autograder for C++ files')
@@ -6,15 +7,24 @@ parser.add_argument('cppFilePath', type=str, help='Path to C++ file')
 parser.add_argument('--test', type=int, required=False, help='Test number')
 
 args = parser.parse_args()
+tests = []
 
 # read cpp file and parse tests from file
 with open(args.cppFilePath) as cppFile:
   cppFileLines = cppFile.readlines()
 
   # parse tests from cpp file
-  tests = []
-  for line in cppFileLines:
-    if line.lower().startswith('// test'):
-      tests.append(line)
+  for i in range(len(cppFileLines)):
+    if cppFileLines[i].lower().startswith('// test'):
+      tests.append(cppFileLines[i])
+      tests.append(cppFileLines[i+1])
+      tests.append(cppFileLines[i+2])
 
-  print(tests)
+result = subprocess.run(['g++', args.cppFilePath], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if result.returncode != 0:
+  print('Compilation failed')
+  print(result.stderr.decode('utf-8'))
+  exit(1)
+
+result2 = subprocess.run(['./' + args.cppFilePath[:-4]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+print(result2.stdout.decode('utf-8'))
